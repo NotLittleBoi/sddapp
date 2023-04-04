9e782d3b0a6a53a85b5ab789a8d18c73afc6eea4
NotLittleBoi <fox33108@gmail.com>
2023-04-04 09:17:42 -0400

    fix issues

---
+++ Login.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Login() {
+    return (
+        <View style = {styles.container}>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+    },
+});
